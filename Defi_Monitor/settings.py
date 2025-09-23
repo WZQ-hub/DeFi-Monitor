76d9594eb189a6ec1b98ca968c00ae82d77cd780
SUPER_WZQ <super1wzq1@outlook.com>
2025-09-24 02:12:26 +0800

    连接postgresql
    Signed-off-by: SUPER_WZQ <super1wzq1@outlook.com>

--- Defi_Monitor/settings.py
+++ Defi_Monitor/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-oo$rj1fo5+ae9&#-b_#$+1a=x485s&t++o=(^$iu)5t6(23@f)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'Defi_Monitor.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "super",
+        'USER': "super_wzq",
+        'PASSWORD': "wan20020423",
+        'HOST': "localhost",
+        'PORT': "5432",
     }
 }
 
